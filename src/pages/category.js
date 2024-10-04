edb8591fc866079512b453ac7779bb0d0dc7515b
Aissariya <aissariya.t@ku.th>
2024-10-04 23:36:00 +0700

    9

--- src/pages/category.js
+++ src/pages/category.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Category = () => {
   return (
     <div>
-      <h1>Welcome to the Category page!4444444445555555555</h1>
+      <h1>Welcome to the Category page!99999</h1>
     </div>
   );
 };
