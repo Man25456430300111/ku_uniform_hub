0d5d9eeb7633f3e48c4362ca37228d37b21acc80
jakkapop <jakkapop80@gmail.com>
2024-10-04 22:14:01 +0700

    test1

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
   return (
     <div>
-      <h1>Welcome to the Login page!</h1>
+      <h1>Welcome to the Login page Manendtest1!</h1>
     </div>
   );
 };
