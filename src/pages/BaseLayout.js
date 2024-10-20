235eb8fa985069462d61b3af2486db4ad259cdaa
6430301125 <aissariya.t@ku.th>
2024-10-20 11:45:34 +0700

    changed background

--- src/pages/BaseLayout.js
+++ src/pages/BaseLayout.js
@@ -5,7 +5,7 @@ function BaseLayout({ children }) {
         <div
             style={{
                 height: '100vh',
-                backgroundImage: 'url(https://media.discordapp.net/attachments/1130936978519633940/1295741813239447625/image.png?ex=670fc119&is=670e6f99&hm=920187b7c9a3a082cc6f818d78d445caace451e37745bb183bd2ab8df6f54b16&=&format=webp&quality=lossless&width=1191&height=670)',
+                backgroundImage: 'url(https://i.pinimg.com/control/564x/1d/a9/6e/1da96e4e4612e509637d17fd725c3416.jpg)',
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 display: 'flex',
--- src/pages/category.js
+++ src/pages/category.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+//import ProductCard from './ProductCard';
 
-const Category = () => {
+const CategoryProducts = () => {
+  const { categoryName } = useParams(); // ดึงชื่อประเภทจาก URL
+  const [products, setProducts] = useState([]);
+
+  /*useEffect(() => {
+    // เรียกข้อมูลสินค้าตามประเภทจาก API
+    axios.get(`http://localhost:5000/api/products?category=${categoryName}`)
+      .then(response => setProducts(response.data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, [categoryName]);*/
+
   return (
-    <div>
-      <h1>Welcome to the Category page!000</h1>
+    <div className="category-products">
+      <h2>Products in category: {categoryName}</h2>
+      <div className="product-grid">
+        {products.map(product => (
+          <ProductCard 
+            key={product.id} 
+            name={product.name} 
+            price={product.price} 
+            image={product.image} 
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default Category;
+export default CategoryProducts;
